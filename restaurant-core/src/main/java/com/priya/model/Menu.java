6cc35f3674f06c89f1cfa2ed8cad091aaffb7c23
Priya <Priya@HP-PC>
2016-12-20 15:04:16 +0530

    Restaurant

---
+++ restaurant-core/src/main/java/com/priya/model/Menu.java
@@ -0,0 +1,9 @@
+package com.priya.model;
+
+import lombok.Data;
+
+@Data
+public class Menu {
+	private int id;
+	private String name;
+}
---
+++ restaurant-core/src/main/java/com/priya/model/TestMenu.java
@@ -0,0 +1,21 @@
+package com.priya.model;
+
+import com.priya.model.Menu;
+
+public class TestMenu {
+	public static void main(String[] args) {
+
+		Menu menu = new Menu();
+
+		System.out.println(menu.getId());
+		System.out.println(menu.getName());
+
+		menu.setId(1);
+		menu.setName("Idli");
+
+		System.out.println(menu.toString());
+
+		System.out.println(menu.getId());
+		System.out.println(menu.getName());
+	}
+}
