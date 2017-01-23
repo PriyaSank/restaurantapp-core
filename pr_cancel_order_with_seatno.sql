1e522e2fe8c60a8786cb44bccfefaf193dd55b81
PriyaSank <priyasankaran95@gmail.com>
2017-01-23 12:17:26 +0530

    Add files via upload

---
+++ pr_cancel_order_with_seatno.sql
@@ -0,0 +1,24 @@
+DELIMITER $$
+
+USE `restuarant`$$
+
+DROP PROCEDURE IF EXISTS `PR_CANCEL_BILL_V5`$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `PR_CANCEL_BILL_V5`(IN i_seat_no INT,OUT RES VARCHAR(50))
+BEGIN
+DECLARE l_order_id INT DEFAULT 0;
+SET RES='The order for the seat is:';
+IF EXISTS(SELECT * FROM `seat_configuration` WHERE `ID`=i_seat_no) THEN
+SELECT `ID` INTO l_order_id FROM `order_details` WHERE `SEAT_NO`= i_seat_no AND `STATUS`='INITIATED';
+IF l_order_id IS NOT NULL THEN
+UPDATE `order_details` SET `STATUS`='CANCELLED' WHERE `ID`=l_order_id;
+SET RES=CONCAT(RES,'Order cancelled');
+ELSE
+SET RES=CONCAT(RES,'Order cannot be cancelled for this seat.The order is',`FN_CHECK_ORDER_STATUS`(l_order_id));
+END IF;
+ELSE
+SET RES=CONCAT(RES,'Invalid seat no.Seat no not found');
+END IF;
+END$$
+
+DELIMITER ;
---
+++ pr_customer_order.sql
@@ -0,0 +1,36 @@
+DELIMITER $$
+
+USE `restuarant`$$
+
+DROP PROCEDURE IF EXISTS `PR_GET_CUSTOMER_ORDER_V4`$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `PR_GET_CUSTOMER_ORDER_V4`(IN i_seat_no INT,IN i_menu_items VARCHAR(200),IN i_menu_count VARCHAR(50),OUT message VARCHAR(500))
+BEGIN
+DECLARE l_order_id INT;
+DECLARE l_order_status VARCHAR(20);
+IF (FN_CHECK_ALL_SEAT_FILLED()) THEN
+IF (FN_IS_SEAT_AVAILABLE(i_seat_no)) THEN
+UPDATE seat_configuration SET `STATUS`=1 WHERE ID=i_seat_no;
+INSERT INTO order_details(SEAT_NO) VALUES(i_seat_no);
+SELECT ID INTO l_order_id FROM order_details WHERE `SEAT_NO`=i_seat_no AND `STATUS`='INITIATED';
+SELECT `STATUS` INTO l_order_status FROM `order_details` WHERE `ID`=l_order_id;
+IF (l_order_status='INITIATED') THEN
+UPDATE order_details SET STATUS='SERVED' WHERE ID=l_order_id;
+CALL PR_SPLIT_INPUT_V2(i_menu_items,i_menu_count,l_order_id,i_seat_no,@msg);
+SET message=@msg;
+UPDATE seat_configuration SET STATUS=0 WHERE ID=i_seat_no;
+ELSE
+SET message='The order is cancelled.';
+END IF;
+IF ((SELECT COUNT(*) FROM `order_food_details` WHERE `ORDER_ID`=l_order_id)=0) THEN
+UPDATE order_details SET STATUS='INVALID' WHERE ID=l_order_id;
+END IF;
+ELSE
+SET message='Particular seat not available';
+END IF;
+ELSE
+SET message='All seats filled';
+END IF;
+END$$
+
+DELIMITER ;
