dd4c4fcc1bca5dace79ba7825211a69efad50a70
PriyaSank <priyasankaran95@gmail.com>
2017-01-31 19:42:41 +0530

    Update order_details_table.sql

--- order_details_table.sql
+++ order_details_table.sql
@@ -2,8 +2,9 @@
 
 CREATE TABLE `order_details` (                                                                                                                                                                                                      
                  `ID` int(11) NOT NULL AUTO_INCREMENT,                                                                                                                                                                                             
-                 `SEAT_NO` int(11) DEFAULT NULL,                                                                                                                                                                                                   
+                 `SEAT_NO` int(11) DEFAULT NOT NULL,                                                                                                                                                                                                   
                  `STATUS` varchar(30) DEFAULT 'INITIATED',                                                                                                                                                                                         
-                 PRIMARY KEY (`ID`)                                                                                                                                                                                                                
+                 PRIMARY KEY (`ID`),
+                 CONSTRAINT `FK_SEAT` FOREIGN KEY (`SEAT_NO`) REFERENCES `seat_configuration` (`ID`)
                )
 
